ff23536993ff2180437c20eae163bc3cc2f4994e
arapaill <arapaill@student.s19.be>
2022-03-14 15:20:11 +0100

    added destructor, overload operator=, top() and top() const

--- Stack.hpp
+++ Stack.hpp
@@ -10,8 +10,22 @@ class stack
 public:
 	// Constructeur
 	// Destructeur
-	// Operator ==
+    ~stack();
+	// Operator =
+    stack& operator=( const stack& other );
+    {
+        c = other.c;
+        return(*this);
+    }
 	// Top
+    reference top();
+    { 
+         return (this->ct.back());
+    }
+    const_reference top() const;
+    { 
+         return (this->ct.back());
+    }
 	// Empty
 	// Size
 	// Push
