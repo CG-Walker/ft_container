336ac3eff204d5b974912cd81f5a3a072c77bddc
CG-Walker <cogo97@hotmail.com>
2022-03-14 16:34:28 +0100

    Test de stack

--- tester_stack.cpp
+++ tester_stack.cpp
@@ -1,11 +1,28 @@
 #include "Stack.hpp"
 #include "tester.hpp"
-#include <stack>
 
+#include <stack>
+#include <iostream>
 
 int main(void)
 {
-    std::stack<int> std_stack;
-    ft::stack<int> ft_stack;
-    
+	std::stack<int> std_stack;
+	ft::stack<int> ft_stack;
+
+	std_stack.push(42);
+	ft_stack.push(42);
+
+	std::cout << std_stack.top() << std::endl;
+	std::cout << ft_stack.top() << std::endl;
+	std::cout << std_stack.size() << std::endl;
+	std::cout << ft_stack.size() << std::endl;
+
+	std_stack.pop();
+	ft_stack.pop();
+
+	std::cout << std_stack.empty() << std::endl;
+	std::cout << ft_stack.empty() << std::endl;
+
+	std::cout << std_stack.size() << std::endl;
+	std::cout << ft_stack.size() << std::endl;
 }
