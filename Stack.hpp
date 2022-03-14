cb6c19bc0fe53a7b46b305fc731ee538165138d9
arapaill <arapaill@student.s19.be>
2022-03-14 15:51:15 +0100

    bug fix compil

--- Stack.hpp
+++ Stack.hpp
@@ -12,7 +12,7 @@ public:
 	typedef typename Container::value_type			value_type;
 	typedef typename Container::size_type			size_type;
 	typedef typename Container::reference			reference;
-	typedef typename Container::constr_reference	const_reference;
+	typedef typename Container::const_reference	const_reference;
 
 	// Constructors, Destructor & Operator
 	explicit stack( const Container & cont = Container() );
@@ -20,7 +20,7 @@ public:
 
 	~stack();
 
-	stack & operator=( const stack & other );
+	stack & operator=( const stack & other )
 	{
 		this->c = other.c;
 		return(*this);
@@ -53,7 +53,7 @@ public:
 	// Modifiers
 	void push( const value_type & value )
 	{
-		this->c.push_back(value)
+		this->c.push_back(value);
 	}
 
 	void pop()
