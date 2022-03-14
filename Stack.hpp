4627552b0be3756d3576a17e9b26bf8237aa2296
CG-Walker <cogo97@hotmail.com>
2022-03-14 15:00:45 +0100

    Base de stack

---
+++ Stack.hpp
@@ -0,0 +1,26 @@
+#include <vector>
+
+namespace ft
+{
+
+
+template <class T, class Container = std::vector<T>> 
+class stack
+{
+public:
+	// Constructeur
+	// Destructeur
+	// Operator ==
+	// Top
+	// Empty
+	// Size
+	// Push
+	// Pop
+private:
+
+}; // Class Vector
+
+// Fonctions non membre :
+// == != < <= > >=
+
+} // Namespace FT
