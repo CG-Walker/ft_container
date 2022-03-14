5deae021b3488ed0f31afec8f53d6b79c05769d1
CG-Walker <cogo97@hotmail.com>
2022-03-14 16:53:49 +0100

    Base de Vector.hpp

--- Vector.hpp
+++ Vector.hpp
@@ -5,47 +5,44 @@
 
 namespace ft
 {
+	template <class T, class Allocator = std::allocator<T>>
+	class vector
+	{
+		public:
+			// Member types
+			typedef T											value_type;
+			typedef Allocator									allocator_type;
+			typedef typename allocator_type::size_type			size_type;
+			typedef typename allocator_type::difference_type	difference_type;
+			typedef typename allocator_type::reference			reference;
+			typedef typename allocator_type::const_reference	const_reference;
+			typedef typename allocator_type::pointer			pointer;
+			typedef typename allocator_type::const_pointer		const_pointer;
+			//typedef typename iterator							iterator;		// À implémenter
+			//typedef typename const_iterator					const_iterator;	// À implémenter
+			//typedef typename reverse_iterator					iterator;		// À implémenter
+			//typedef typename const_reverse_iterator			const_iterator;	// À implémenter
 
-template <typename T, class Allocator = std::allocator<T>>
-class Vector
-{
-	typedef T											value_type;
-	typedef Allocator									allocator_type;
-	typedef typename allocator_type::reference			reference;
-	typedef typename allocator_type::const_reference	const_reference;
-	typedef typename allocator_type::pointer			pointer;
-	typedef typename allocator_type::const_pointer		const_pointer;
-	typedef typename allocator_type::size_type			size_type;
-public:
-	// Constructors & Destructors
-	explicit Vector(const allocator_type & alloc = allocator_type()) : _alloc(alloc), _ptr(0), _size(0), _capacity(0) {};
-	Vector(Vector & src) : _alloc(allocator_type()), _ptr(0), _size(0), _capacity(0) { *this = src; };
-	virtual ~Vector() {
-		// clear();
-		if (_capacity > 0)
-		{
-			_alloc.deallocate(_ptr, _capacity);
-			_capacity = 0;
-		}
-	};
 
-	// Iterators
+			// Constructors, Destructor & Operator
 
-	// Capacity
+			// Element access
 
-	// Element access
+			// Iterators
 
-	// Modifiers
+			// Capacity
 
-	// Allocator
-	void clear() {};
-private:
-	allocator_type	_alloc;
-	pointer 		_ptr;
-	size_t			_size;
-	size_t			_capacity;
-}; // class Vector
+			// Modifiers
 
- #endif
+		private:
+			allocator_type	_alloc;
+			pointer 		_ptr;
+			size_t			_size;
+			size_t			_capacity;
+	}; // class Vector
+
+	// Non-member functions
 
 } // namespace ft
+
+ #endif
