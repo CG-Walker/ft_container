2c17f59108e471f92703f6b1affedf35ad99027d
CG-Walker <cogo97@hotmail.com>
2022-03-14 15:21:55 +0100

    Ajout de Constructor, empty, size

--- Stack.hpp
+++ Stack.hpp
@@ -3,17 +3,36 @@
 namespace ft
 {
 
-
-template <class T, class Container = std::vector<T>> 
+template <class T, class Container = std::vector<T>>
 class stack
 {
 public:
+
+	typedef Container								container_type;
+	typedef typename Container::value_type			value_type;
+	typedef typename Container::size_type			size_type;
+	typedef typename Container::reference			reference;
+	typedef typename Container::constr_reference	const_reference;
+
 	// Constructeur
+	explicit stack( const Container & cont = Container() );
+	stack( const stack & other );
 	// Destructeur
-	// Operator ==
+	// Operator =
 	// Top
 	// Empty
+	bool empty() const
+	{
+		if (size() == 0)
+			return (true);
+		else
+			return (false);
+	};
 	// Size
+	size_type size() const
+	{
+		return (c.size());
+	}
 	// Push
 	// Pop
 private:
