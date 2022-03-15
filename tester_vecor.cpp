a9acb6e8e072a92303ef842157a2beb1e7b728e4
CG-Walker <cogo97@hotmail.com>
2022-03-15 16:02:08 +0100

    Ajout de push_back et modification de l'utilisation des pointers dans vector

--- tester_vecor.cpp
+++
@@ -1,8 +0,0 @@
-#include "Vector.hpp"
-
-int main()
-{
-   ft::vector<int> ft_vec;
-
-   ft_vec.reserve(20);
-}
--- tester_vector.cpp
+++ tester_vector.cpp
@@ -1,11 +1,24 @@
 #include "Vector.hpp"
+#include <iostream>
+#include <vector>
 
 int main()
 {
-   ft::vector<int>		ft_vec;
-   ft::iterator<const int>	ft_it;
+	std::vector<int>		std_vec;
+	ft::vector<int>		ft_vec;
+	ft::iterator<int>	   ft_it;
 
-   ft_vec.reserve(20);
-   ft_it = ft_vec.begin();
-   
+	//ft_vec.reserve(20);
+
+	ft_vec.push_back(42);
+	ft_vec.push_back(45);
+	ft_vec.push_back(487);
+	ft_vec.push_back(21);
+
+	std_vec.push_back(42);
+
+	for (ft::iterator<int> it = ft_vec.begin(); it != ft_vec.end(); it++)
+	{
+		std::cout << "first : " << *it << std::endl;
+	}
 }
