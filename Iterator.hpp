2dfc27eb4ef2d2949d401fc9233a49d459073d00
arapaill <arapaill@student.s19.be>
2022-03-15 11:31:15 +0100

    Start of Iteratop.hpp

---
+++ Iterator.hpp
@@ -0,0 +1,28 @@
+#ifndef ITERATOR_HPP
+# define ITERATOR_HPP
+
+namespace ft
+{
+    template <typename T>
+	class Iterator
+    {
+        public:
+            typedef std::random_access_iterator_tag iterator_category;
+            typedef T                               value_type;
+            typedef T*                              pointer;
+            typedef T&                              reference;
+            typedef std::ptrdiff_t                  difference_type;
+
+            // constructor and destructor
+            Iterator(void) : _ptr(NULL) {};
+            Iterator(const Iterator & other) {};
+            ~Iterator(void) {};
+
+        private:
+            pointer _ptr;
+            Iterator(pointer elem) : _ptr(elem);
+            
+    }
+}
+
+#endif
