a267ba110d78668ec07071c36a259621147b4fa7
CG-Walker <cogo97@hotmail.com>
2022-03-15 16:03:26 +0100

    Ajout de push_back et modification de l'utilisation des pointers dans vector

--- Vector.hpp
+++ Vector.hpp
@@ -31,8 +31,7 @@ namespace ft
 			//typedef typename const_reverse_iterator			const_iterator;		// À implémenter
 
 			// Constructors, Destructor & Operator
-			vector() : _ptr(0), _size(0), _capacity(0) {};
-			explicit vector( const Allocator & alloc );
+			explicit vector( const Allocator & alloc = allocator_type()) : _alloc(alloc), _first(NULL), _last(NULL), _capacity(0) {};
 			explicit vector( size_type count, const T & value = T(), const Allocator & alloc = Allocator());
 			vector( const vector & other );
 			
@@ -50,51 +49,50 @@ namespace ft
 			// Element access
 			reference at( size_type pos ) 
 			{
-				if (!(pos < this->_size))
+				if (!(pos < this->size()))
 					throw (std::out_of_range("vector::at: "));
 				return (*this[pos]);
 			}
 
 			const_reference at( size_type pos ) const
 			{
-				if (!(pos < this->_size))
+				if (!(pos < this->size()))
 					throw (std::out_of_range("vector::at: "));
 				return (*this[pos]);
 			}
 
-			reference operator[]( size_type pos ) {	return *(this->_ptr + pos);	};
+			reference operator[]( size_type pos ) {	return *(this->_first + pos);	};
 
 			// Iterators
-			iterator begin() { return iterator(this->_ptr); };
+			iterator begin() { return iterator(this->_first); };
 
-			const_iterator begin() const { return const_iterator(this->_ptr); };
+			const_iterator begin() const { return const_iterator(this->_first); };
 
-			iterator end() { return iterator(this->_ptr + this->_size); };
+			iterator end() { return iterator(this->_last); };
 
-			const_iterator end() const { return const_iterator(this->_ptr + this->_size); };
+			const_iterator end() const { return const_iterator(this->_last); };
 
 			// Capacity
-			bool empty() const { return !(this->_size); };
+			bool empty() const { return (this->_first == _last); };
 
-			size_type size() const { return (this->_size); };
+			size_type size() const { return (this->_last - this->_first); };
 
 			size_type max_size() const; // À implémenter
 
 			void reserve( size_type new_cap )
 			{
-				if (new_cap <= this->_capacity || new_cap == 0)
-					return ;
-				allocator_type alloc;
-				pointer new_ptr;
-				new_ptr = alloc.allocate(new_cap);
-				for (size_type i = 0; i < this->_size; i++)
+				if (new_cap > this->_capacity)
 				{
-					alloc.construct(& new_ptr[i], this->_ptr[i]);
-					alloc.destroy(& this->_ptr[i]);
+					pointer new_first;
+					new_first = this->_alloc.allocate(new_cap);
+					size_type old_size = this->size();
+					std::uninitialized_copy(this->_first, this->_last, new_first);
+					this->clear();
+					this->_alloc.deallocate(this->_first, this->_capacity);
+					this->_first = new_first;
+					this->_last = this->_first + old_size;
+					this->_capacity = new_cap;
 				}
-				alloc.deallocate(this->_ptr, this->_capacity);
-				this->_ptr = new_ptr;
-				this->_capacity = new_cap;
 			}
 
 			size_type capacity() const { return (this->_capacity); };
@@ -102,12 +100,19 @@ namespace ft
 			// Modifiers
 			void clear()
 			{
-				allocator_type alloc;
-				for (size_type i = 0; i < this->_size; i++)
+				for (pointer now = this->_first; now != this->_last; now++)
 				{
-					alloc.destroy(this->_ptr[i]);
+					this->_alloc.destroy(now);
 				}
-				this->_size = 0;
+			}
+
+			void push_back( const value_type & value )
+			{
+				size_type new_size = this->size() + 1;
+				if (new_size > this->_capacity)
+					this->reserve(calc_new_capacity(new_size));
+				this->_alloc.construct(this->_last, value);
+				this->_last++;
 			}
 
 			iterator insert( iterator pos, const value_type & value );
@@ -116,15 +121,46 @@ namespace ft
 			{
 				size_type new_size = this->size() + count;
 				size_type offset = pos - begin();
-				std::cout << offset << std::endl;
+				
+				if (new_size > this->_capacity)
+				{
+					std::cout << "TODO\n";
+				}
+				else
+				{
+					for (size_type i = 0; i < count; i++)
+					{
+						if (offset + i >= this->size())
+						{
+							this->_alloc.construct(&this->_first[offset + i], value);
+						}
+						else
+						{
+							this->_first[offset + 1] = value;
+						}
+					}
+					
+				}
 			}
 
 			//template <class InputIt>
 			//void insert( iterator pos, InputIt first, InputIt last );
 
+ 			size_type calc_new_capacity(size_type new_cap)
+        	{
+				size_type max_cap = 500;//max_size();
+				if (new_cap > max_cap)
+					throw std::length_error("allocator<T>::allocate(size_t n) 'n' exceeds maximum supported size");
+				size_type cap = this->_capacity;
+				if (cap >= max_cap / 2)
+					return max_cap;
+				return std::max(cap * 2, new_cap);
+       		}
+
 		private:
-			pointer			_ptr;
-			size_type		_size;
+			allocator_type	_alloc;
+			pointer			_first;
+			pointer			_last;
 			size_type		_capacity;
 	}; // class Vector
 
