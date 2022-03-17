9f1b9d1094c859d4bc667602161746ef1c4510b4
arapaill <arapaill@student.s19.be>
2022-03-17 13:43:54 +0100

    added pair in Utils.hpp

--- Utils.hpp
+++ Utils.hpp
@@ -32,30 +32,90 @@ namespace ft
 		return true;
 	}
 
+	template <class T1, class T2>
+	struct pair
+	{
+		typedef T1 first_type;
+		typedef T2 second_type;
+
+		first_type  first;
+		second_type	second;
+		pair() : first(), second() {};
+
+		template <class U, class V>
+       	pair(const pair<U, V> &pr) : first(pr.first), second(pr.second) {};
+
+		pair(const first_type &a, const second_type &b) : first(a), second(b) {};
+
+		pair &operator=(const pair &pr)
+       	{
+        	first = pr.first;
+    		second = pr.second;
+            return *this;
+        }
+	}
+	template <class T1, class T2>
+    bool operator==(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs)
+    {
+        return lhs.first == rhs.first && lhs.second == rhs.second;
+    }
+	template <class T1, class T2>
+    bool operator!=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs)
+    {
+        return !(lhs == rhs);
+    }
+    template <class T1, class T2>
+    bool operator<(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs)
+    {
+        return lhs.first < rhs.first ||
+               (!(rhs.first < lhs.first) && lhs.second < rhs.second);
+    }
+    template <class T1, class T2>
+    bool operator<=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs)
+    {
+        return !(rhs < lhs);
+    }
+    template <class T1, class T2>
+    bool operator>(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs)
+    {
+        return rhs < lhs;
+    }
+    template <class T1, class T2>
+    bool operator>=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs)
+    {
+        return !(lhs < rhs);
+    }
+
+    template <class T1, class T2>
+    pair<T1, T2> make_pair(T1 x, T2 y)
+    {
+        return (pair<T1, T2>(x, y));
+    }
+	
 	// Simple copie non-comprise pour l'instant !!
 	template <class T>
-    struct remove_cv
-    {
-        typedef T type;
-    };
+	struct remove_cv
+	{
+		typedef T type;
+	};
 
-    template <class T>
-    struct remove_cv<const T>
-    {
-        typedef T type;
-    };
+	template <class T>
+	struct remove_cv<const T>
+	{
+		typedef T type;
+	};
 
-    template <class T>
-    struct remove_cv<volatile T>
-    {
-        typedef T type;
-    };
+	template <class T>
+	struct remove_cv<volatile T>
+	{
+		typedef T type;
+	};
 
-    template <class T>
-    struct remove_cv<const volatile T>
-    {
-        typedef T type;
-    };
+	template <class T>
+	struct remove_cv<const volatile T>
+	{
+		typedef T type;
+	};
 	// -------------------------------------------------
 
 	template <bool B, class T = void>
@@ -74,7 +134,7 @@ namespace ft
 	};
 
 	typedef integral_constant<bool, true> true_type;
-    typedef integral_constant<bool, false> false_type;
+	typedef integral_constant<bool, false> false_type;
 
 	template <class T>
 	struct is_integral_helper : public false_type {};
