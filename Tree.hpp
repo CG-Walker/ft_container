ab8f05763cf325518456968c93c129870505521d
arapaill <arapaill@student.s19.be>
2022-03-22 16:23:43 +0100

    find fonction

--- Tree.hpp
+++ Tree.hpp
@@ -111,17 +111,21 @@ namespace ft
                 link_type root = this->_current;
                 link_type ret = this->_currents;
                 
-                while (ret != value)
+                if (ret != value)
                 {
-                	if (this->_current == value)
+                    while (true)
                     {
-                        ret = this->_current;
-                        this->_current = root;
+                        if (this->_current == value)
+                        {
+                            ret = this->_current;
+                            this->_current = root;
+                            break ;
+                        }
+                        else if (value >= this->_current->value) // tjrs a verifier
+                            this->_current = this->_current->right;
+                        else if (value < this->_current->value)
+                            this->_current = this->_current->left;
                     }
-                    else if (value >= this->_current->value) // tjrs a verifier
-                        this->_current = this->_current->right;
-                    else if (value < this->_current->value)
-                        this->_current = this->_current->left;
                 }
                 return (ret);
             }
