53f1debe880362f5aadaad04a73200a31b2e2ab1
arapaill <arapaill@student.s19.be>
2022-03-31 11:46:32 +0200

    added reverse iterator

--- Tree.hpp
+++ Tree.hpp
@@ -61,10 +61,10 @@ namespace ft
 			const_iterator begin() const { return const_iterator(this->_begin, this->_nil); }
 			iterator end() { return iterator(this->_end, this->_nil); }
 			const_iterator end() const { return const_iterator(this->_end, this->_nil); }; // A corriger
-			//reverse_iterator rbegin();
-			//const_reverse_iterator rbegin() const;
-			//reverse_iterator rend();
-			//const_reverse_iterator rend() const;
+			reverse_iterator rbegin() {return reverse_iterator(end());};
+			const_reverse_iterator rbegin() const {return const_reverse_iterator(end());};
+			reverse_iterator rend() {return reverse_iterator(begin());};
+			const_reverse_iterator rend() const {return const_reverse_iterator(begin());};
 
 			// Capacity
 			size_type size() const { return (this->_size); };
