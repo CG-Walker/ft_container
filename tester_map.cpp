e714386e329e84b7b8c54c5596bf4687254ffa79
CG-Walker <cogo97@hotmail.com>
2022-04-25 15:45:21 +0200

    Ajouts de tests

--- tester_map.cpp
+++ tester_map.cpp
@@ -4,10 +4,143 @@
 #include "Utils.hpp"
 #include <iostream>
 
+// TEST DES FONCTIONS ITERATORS
+void	tests_iterators()
+{
+	std::cout << "TESTS DES FONCTIONS ITERATORS\n";
+
+	ft::map<int, int> map_iterators;
+	ft::map<int, int>::iterator it;
+	ft::map<int, int>::reverse_iterator r_it;
+
+	map_iterators.insert(ft::pair<int, int>(10, 1));
+	map_iterators.insert(ft::pair<int, int>(9, 1));
+	map_iterators.insert(ft::pair<int, int>(8, 1));
+
+	map_iterators.print_tree("map_iterators");
+
+	it = map_iterators.begin();
+	std::cout << "\nbegin() : " << it.base() << std::endl;
+	it = map_iterators.end();
+	std::cout << "end() : " << it.base() << std::endl;
+
+	// Ne fonctionne pas
+	/* r_it = map_iterators.rbegin();
+	std::cout << "rbegin() : " << r_it.base() << std::endl;
+	r_it = map_iterators.rend();
+	std::cout << "rend() : " << r_it.base() << std::endl; */
+
+	std::cout << "\n";
+}
+
+void	tests_capacity()
+{
+	std::cout << "TESTS DES FONCTIONS CAPACITY\n";
+
+	ft::map<int, int> map_capacity;
+
+	std::cout << "empty() Is map_capacity empty ? " << (map_capacity.empty() ? "Yes" : "No") << std::endl;
+	map_capacity.insert(ft::pair<int, int>(10, 1));
+	std::cout << "empty() Is map_capacity empty ? " << (map_capacity.empty() ? "Yes" : "No") << std::endl;
+
+	std::cout << "size() : " << map_capacity.size() << std::endl;
+	std::cout << "max_size() : " << map_capacity.max_size() << std::endl;
+
+	std::cout << "\n";
+}
+
+void	tests_modifiers()
+{
+	std::cout << "TESTS DES FONCTIONS MODIFIERS\n";
+
+	ft::map<int, int> map_modifiers;
+
+	std::cout << "insert()...\n";
+	map_modifiers.insert(ft::pair<int, int>(10, 1));
+	map_modifiers.insert(ft::pair<int, int>(9, 1));
+	map_modifiers.insert(ft::pair<int, int>(8, 1));
+
+	// Tester les deux autres inserts.
+
+	map_modifiers.print_tree("map_modifiers");
+
+	// Ne fonctionne pas
+	std::cout << "erase()...\n";
+	//map_modifiers.erase(map_modifiers.begin());
+	//map_modifiers.erase(8);
+
+	map_modifiers.print_tree("map_modifiers");
+
+	ft::map<int, int> map_modifiers_swap;
+
+	map_modifiers.insert(ft::pair<int, int>(66, 1));
+	map_modifiers.insert(ft::pair<int, int>(52, 1));
+	map_modifiers.insert(ft::pair<int, int>(85, 1));
+
+	map_modifiers_swap.swap(map_modifiers);
+
+	map_modifiers.print_tree("map_modifiers");
+	map_modifiers_swap.print_tree("map_modifiers_swap");
+
+	// Crash when calling clear on a empty map
+	//map_modifiers.clear();
+	map_modifiers_swap.clear();
+
+	map_modifiers.print_tree("map_modifiers");
+	map_modifiers_swap.print_tree("map_modifiers_swap");
+
+	std::cout << "\n";
+}
+
+void	tests_element_access()
+{
+	std::cout << "TESTS DES FONCTIONS ELEMENT_ACCESS\n";
+
+	ft::map<int, int> map_element_access;
+	ft::map<int, int>::iterator it;
+	ft::pair<ft::map<int, int>::iterator,ft::map<int, int>::iterator> map_pair;
+
+	map_element_access.insert(ft::pair<int, int>(10, 1));
+	map_element_access.insert(ft::pair<int, int>(9, 1));
+	map_element_access.insert(ft::pair<int, int>(8, 1));
+	
+	// Ne fonctionne pas
+	//std::cout << "count(8) : " << map_element_access.count(8) << std::endl;
+	//std::cout << "count(5) : " << map_element_access.count(5) << std::endl;
+
+	// Vérifier la valeur de it
+	it = map_element_access.find(8);
+	std::cout << "find(8) : " << it.base() << std::endl;
+	it = map_element_access.find(42);
+	std::cout << "find(42) : " << it.base() << std::endl;
+
+	// Ne fonctionne pas
+	//map_pair = map_element_access.equal_range(9);
+	//std::cout << "equal_range(9) : " << map_pair.first << std::endl;
+
+	// Ne fonctionne pas
+	//it = map_element_access.lower_bound(8);
+	//std::cout << "lower_bound(8) : " << it.base() << std::endl;
+	//it = map_element_access.upper_bound(9);
+	//std::cout << "upper_bound(9) : " << it.base() << std::endl;
+
+	std::cout << "\n";
+}
+
+void	tests_observers()
+{
+	// TODO
+}
+
 
 int main()
 {
-	ft::map<int, int> ft_map;
+	tests_iterators();
+	tests_capacity();
+	tests_modifiers();
+	tests_element_access();
+
+	/* ft::map<int, int> ft_map;
 	ft::map<char, int> ft_map_char;
 	ft_map.insert(ft::pair<int, int>(10, 1));
 	ft_map.insert(ft::pair<int, int>(9, 1));
@@ -32,7 +165,7 @@ int main()
 	if(ft_empty.empty())
 		std::cout << "\nft_empty is empty" << std::endl;
 	if(!ft_map.empty())
-	std::cout << "ft_map is not empty" << std::endl;
+	std::cout << "ft_map is not empty" << std::endl; */
 
 /* 	ft::map<int, int>::iterator it_b = ft_map.begin();
 	ft::map<int, int>::iterator it_e = ft_map.end();
