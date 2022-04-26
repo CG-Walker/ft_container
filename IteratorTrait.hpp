832cdff3c16e53c33bf22db9cccbd9151dd26028
arapaill <arapaill@student.s19.be>
2022-04-26 16:14:50 +0200

    update traits

--- IteratorTrait.hpp
+++ IteratorTrait.hpp
@@ -1,5 +1,6 @@
 #ifndef ITERATORTRAIT_HPP
 # define ITERATORTRAIT_HPP
+# include "Utils.hpp"
 
 namespace ft
 {
@@ -33,6 +34,113 @@ namespace ft
 		typedef std::random_access_iterator_tag iterator_category;
 	};
 
+    template <class T>
+	struct remove_cv
+	{
+		typedef T type;
+	};
+
+	template <class T>
+	struct remove_cv<const T>
+	{
+		typedef T type;
+	};
+
+	template <class T>
+	struct remove_cv<volatile T>
+	{
+		typedef T type;
+	};
+
+	template <class T>
+	struct remove_cv<const volatile T>
+	{
+		typedef T type;
+	};
+
+
+	template<class Tp, Tp v>
+	struct integral_constant
+	{
+		enum									{ value = v };
+		typedef Tp								value_type;
+		typedef integral_constant<Tp, v>		type;
+		operator value_type() const
+		{
+			return value;
+		}
+		value_type operator()()
+		{
+			return value;
+		}
+	};
+
+	//So true_type is an integral_constant where type is bool and value is true.
+	typedef integral_constant<bool, true>		true_type;
+	typedef integral_constant<bool, false>		false_type;
+
+	template<class Tp>
+	struct _is_integral_helper : public false_type
+	{
+	};
+	template<>
+	struct _is_integral_helper<bool> : public true_type
+	{
+	};
+	template<>
+	struct _is_integral_helper<char> : public true_type
+	{
+	};
+	template<>
+	struct _is_integral_helper<signed char> : public true_type
+	{
+	};
+	template<>
+	struct _is_integral_helper<unsigned char> : public true_type
+	{
+	};
+	template<>
+	struct _is_integral_helper<wchar_t> : public true_type
+	{
+	};
+	template<>
+	struct _is_integral_helper<short> : public true_type
+	{
+	};
+	template<>
+	struct _is_integral_helper<unsigned short> : public true_type
+	{
+	};
+	template<>
+	struct _is_integral_helper<int> : public true_type
+	{
+	};
+	template<>
+	struct _is_integral_helper<unsigned int> : public true_type
+	{
+	};
+	template<>
+	struct _is_integral_helper<long> : public true_type
+	{
+	};
+	template<>
+	struct _is_integral_helper<unsigned long> : public true_type
+	{
+	};
+	template<>
+	struct _is_integral_helper<long long> : public true_type
+	{
+	};
+	template<>
+	struct _is_integral_helper<unsigned long long> : public true_type
+	{
+	};
+
+	// is_integral class inherits from the false or true type, wich is an integral constant
+	template<class Tp>
+	struct is_integral : public _is_integral_helper<typename remove_cv<Tp>::type>::type
+	{
+	};
 } // namespace ft
 
 #endif
